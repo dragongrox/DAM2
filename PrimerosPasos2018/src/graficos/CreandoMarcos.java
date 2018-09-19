6b0751373b838f46280b211b30eec0811ff43f77
drago <dragongrox7@gmail.com>
2018-09-19 14:28:41 +0200

    CFGS

---
+++ PrimerosPasos2018/src/graficos/CreandoMarcos.java
@@ -0,0 +1,25 @@
+package graficos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreandoMarcos {
+
+    public static void main(String[] args) {
+        MiMarco marco1 = new MiMarco();
+        marco1.setVisible(true);
+        marco1.setSize(400, 200);                              //Tamaño de la ventana
+        marco1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);     //Accion realizada al cerrar la ventana
+        marco1.setBounds(50, 200, 500, 100);
+        marco1.setTitle("Mi primera Ventana");
+        marco1.setExtendedState(Frame.MAXIMIZED_BOTH);                      //Ventana maximizada por defecto
+        marco1.setResizable(false);                                         //Deshabilitar maximizar ventana
+    }
+}
+
+class MiMarco extends JFrame {
+    //creamos el constructor
+    public MiMarco() {
+        setLocation(50, 300);
+    }
+}
---
+++ PrimerosPasos2018/src/graficos/CreandoMarcos2.java
@@ -0,0 +1,23 @@
+package graficos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreandoMarcos2 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MiMarco2 extends JFrame {
+    //creamos el constructor
+    public MiMarco2() {
+        setVisible(true);
+        setSize(400, 200);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(50, 200, 500, 100);
+        setTitle("Mi primera Ventana");
+        setExtendedState(Frame.MAXIMIZED_BOTH);
+        setResizable(false);
+    }
+}
