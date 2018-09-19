883dc7ed10ff190b1eaad2abe20a0e488034a9c0
dragongrox <31999215+dragongrox@users.noreply.github.com>
2018-09-20 01:10:56 +0200

    asd

--- practicasPropuestas/src/PrimeraPracticaPropuesta.java
+++ practicasPropuestas/src/PrimeraPracticaPropuesta.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class PrimeraPracticaPropuesta {
+    public static void main(String[] args) {
+        JFrame marco = new JFrame("Primera Practica Propuesta");
+        marco.setVisible(true);
+        marco.setSize(400, 200);
+        marco.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        marco.setBounds(200, 300, 500, 300);
+
+    }
 }
