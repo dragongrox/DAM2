c98304b77ab03a7bd4771ea516d7306d750c3bc9
Dragon <dragongrox7@gmail.com>
2018-09-20 10:43:08 +0200

    DI

---
+++ DI/src/practicasPropuestas/SegundaBisPracticaPropuesta.java
@@ -0,0 +1,4 @@
+package practicasPropuestas;
+
+public class SegundaBisPracticaPropuesta {
+}
---
+++ DI/src/practicasPropuestas/SegundaPracticaPropuesta.java
@@ -0,0 +1,11 @@
+package practicasPropuestas;
+
+import javax.swing.*;
+
+public class SegundaPracticaPropuesta {
+
+    public static void main(String[] args) {
+        JFrame ventana= new JFrame();
+    }
+
+}
