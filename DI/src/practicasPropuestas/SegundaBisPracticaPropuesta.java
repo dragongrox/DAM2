8c5e9d8684fd2caaee9f8b63e6bb78890cae12bb
drago <dragongrox7@gmail.com>
2018-09-20 13:45:20 +0200

    DI

--- DI/src/practicasPropuestas/SegundaBisPracticaPropuesta.java
+++ DI/src/practicasPropuestas/SegundaBisPracticaPropuesta.java
@@ -1,4 +1,24 @@
 package practicasPropuestas;
 
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Crea una ventana, cuyo titulo sea SegundaBis Practica Propuesta, el tamaño sea la resolucion de la pantalla
+ * donde se ejecute y el icono la imagen que tu quieras.
+ */
 public class SegundaBisPracticaPropuesta {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setTitle("SegundaBis Practica Propuesta");
+        Dimension dimension = new Dimension();
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        dimension = toolkit.getScreenSize();
+        frame.setSize(dimension);
+        Image image = toolkit.getImage("C:\\Users\\drago\\IdeaProjects\\DAM2\\DI\\src\\practicasPropuestas\\IconoAlandaluz.jpg");
+        frame.setIconImage(image);
+    }
+
 }
--- DI/src/practicasPropuestas/SegundaPracticaPropuesta.java
+++ DI/src/practicasPropuestas/SegundaPracticaPropuesta.java
@@ -1,11 +1,26 @@
 package practicasPropuestas;
-
+/**
+ * Crea una ventana, cuyo titulo sea Segunda Practica Propuesta, define el tamaño y la posicion con un solo
+ * metodo, tamaño 500,300 y posicion 200, 300. Que la ventana no sea redimensionable. Cambia el icono de la
+ * ventana por el logotipo de ies.
+ */
 import javax.swing.*;
+import java.awt.*;
 
 public class SegundaPracticaPropuesta {
 
     public static void main(String[] args) {
-        JFrame ventana= new JFrame();
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Segunda Practica Propuesta");
+        frame.setBounds(200, 300, 500, 300);
+        frame.setResizable(false);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.getImage("C:\\Users\\drago\\IdeaProjects\\DAM2\\DI\\src\\practicasPropuestas\\IconoAlandaluz.jpg");
+        frame.setIconImage(image);
+
     }
 
 }
