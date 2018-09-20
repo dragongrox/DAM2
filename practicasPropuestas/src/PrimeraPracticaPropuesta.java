7badafc157f8fe5f747cac9448800fb807587a02
drago <dragongrox7@gmail.com>
2018-09-20 09:22:56 +0200

    CFGS

--- practicasPropuestas/src/PrimeraPracticaPropuesta.java
+++ practicasPropuestas/src/PrimeraPracticaPropuesta.java
@@ -5,7 +5,6 @@ public class PrimeraPracticaPropuesta {
     public static void main(String[] args) {
         JFrame marco = new JFrame("Primera Practica Propuesta");
         marco.setVisible(true);
-        marco.setSize(400, 200);
         marco.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         marco.setBounds(200, 300, 500, 300);
 
