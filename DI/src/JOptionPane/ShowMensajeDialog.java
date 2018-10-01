74dca3d8091860229c671447139e17197f256046
dragongrox <31999215+dragongrox@users.noreply.github.com>
2018-10-01 13:16:15 +0200

    DI

---
+++ DI/src/JOptionPane/ShowMensajeDialog.java
@@ -0,0 +1,10 @@
+package JOptionPane;
+
+import javax.swing.*;
+
+public class ShowMensajeDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Metodo con dos argumentos");
+
+    }
+}
