8a6e74ea0da62ab0697fb6d1591bc65960975f71
dragongrox <31999215+dragongrox@users.noreply.github.com>
2018-10-07 14:13:49 +0200

    HLC

--- PSP/PracticaITreads/src/Ejercicio1.java
+++ PSP/PracticaITreads/src/Ejercicio1.java
@@ -1,15 +1,77 @@
+import static java.lang.Thread.sleep;
+
 /**
  * Hacer un programa donde desde un hilo nos muestren los primeros ‘n’ números primos donde n>10 lo pasaremos como
  * parámetros y en otro hilo muestre una cuenta atrás de 15s a 1s. Tiempo de ejecución de cada hilo y del programa
  */
 public class Ejercicio1 extends Thread {
-    public Ejercicio1(String s) {
-        super(s);
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new numPrimos());
+        Thread thread2 = new Thread(new cuentaAtras());
+        long ti = System.currentTimeMillis();
+        thread1.start();
+        thread2.start();
+        while (thread1.isAlive() || thread2.isAlive()) {
+        }
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("Final de Programa, tiempo de Ejecucion:" + te);
+    }
+}
+
+class numPrimos implements Runnable {
+
+    public static void mostrarNPrimos(int n) {
+        int i, nx = 4, cont = 2;
+        String cad = "";
+        if (n > 2) {
+            cad = "2 - 3";
+            while (cont < n) {
+                i = 2;
+                while (i <= nx) {
+                    if (i == nx) {
+                        cad = cad + " - " + nx;
+                        cont = cont + 1;
+                    } else {
+                        if (nx % i == 0) {
+                            i = nx;
+                        }
+                    }
+                    i = i + 1;
+                }
+                nx = nx + 1;
+            }
+            System.out.println(cad);
+        } else {
+            if (n > 0) {
+                if (n == 1) {
+                    System.out.println("es primo 2");
+                } else {
+                    System.out.println("es primo 2, 3");
+                }
+            } else {
+                System.out.println("ingresa numeros positivos");
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        Ejercicio1 ca = new Ejercicio1("hilo1");
-        ca.start(); //lanzo un hilo el programa sigue en ejecucion
+    @Override
+    public void run() {
+        long ti = System.currentTimeMillis();
+        mostrarNPrimos(10000);
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("tiempo de ejecucion primos:" + te);
+    }
+}
+
+class cuentaAtras implements Runnable {
+    public static void cuentaAtras(int num) {
+        int cont = 0;
+        while (cont < num) {
+            dormir(1000);
+            System.out.println((num - cont));
+            cont++;
+
+        }
     }
 
     public static void dormir(long t) {
@@ -22,17 +84,9 @@ public class Ejercicio1 extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Calculando numeros n");
-        for (int cont = 1, n = 0; n <= 1000; cont++) {
-            boolean primo = true;
-            for (int div = 2; div < cont && primo == true; div++) {
-                Float sol = Float.valueOf(cont % div);
-                if (sol == 0) {
-                    primo = false;
-                    System.out.println(n + " es un numero primo");
-                    n++;
-                }
-            }
-        }
+        long ti = System.currentTimeMillis();
+        cuentaAtras(15);
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("tiempo de ejecucion cuenta atras:" + te);
     }
 }
--- PSP/PracticaITreads/src/Ejercicio2.java
+++ PSP/PracticaITreads/src/Ejercicio2.java
@@ -1,3 +1,106 @@
-public class Ejercicio2 {
+import static java.lang.Thread.sleep;
 
+/**
+ * Hacer un programa que pasado un número ‘n’ 100.000<n<1000.000 me los descomponga en factores primos
+ * la salida será tipo n=a² x b⁴ x ….x 1 esto lo hará en un hilo, otro hilo me ira mostrando un saludo
+ * un número de veces pasado como parámetro (entre 1 y 10) separando cada saludo por 1s y otro último
+ * hilo me mostrara 10 veces separadas por un segundo “Java es Divertido”. Tiempo de ejecución de cada
+ * hilo y del programa
+ */
+public class Ejercicio2 extends Thread {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new DescomponerFactores());
+        Thread thread2 = new Thread(new Saludo());
+        Thread thread3 = new Thread(new JavaNoEsDivertido());
+        long ti = System.currentTimeMillis();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        while (thread1.isAlive() || thread2.isAlive()) {
+        }
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("Final de Programa, tiempo de Ejecucion:" + te);
+    }
+}
+
+class DescomponerFactores implements Runnable {
+
+    static void factor(int numero) {
+        int num = 2;
+        String salida = "n=";
+        while (numero != 1) {
+            while (numero % num == 0) {
+                salida = salida + num + "x";
+                numero /= num;
+            }
+            num++;
+        }
+
+        System.out.println(salida.substring(0, (salida.length() - 1)));
+    }
+
+    @Override
+    public void run() {
+        long ti = System.currentTimeMillis();
+        factor(1000000);
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("tiempo de ejecucion factores:" + te);
+    }
+}
+
+class Saludo implements Runnable {
+    public static void cuentaAtras(int num) {
+        int cont = 0;
+        while (cont < num) {
+            dormir(1000);
+            System.out.println(("Hola"));
+            cont++;
+
+        }
+    }
+
+    public static void dormir(long t) {
+        try {
+            sleep(t);
+        } catch (InterruptedException ex) {
+            System.err.println("Error, mensaje : " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        long ti = System.currentTimeMillis();
+        cuentaAtras(10);
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("tiempo de ejecucion Saludos:" + te);
+    }
+}
+
+class JavaNoEsDivertido implements Runnable {
+
+    public static void cuentaAtras(int num) {
+        int cont = 0;
+        while (cont < num) {
+            dormir(1000);
+            System.out.println(("Java es Divertido"));
+            cont++;
+
+        }
+    }
+
+    public static void dormir(long t) {
+        try {
+            sleep(t);
+        } catch (InterruptedException ex) {
+            System.err.println("Error, mensaje : " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        long ti = System.currentTimeMillis();
+        cuentaAtras(10);
+        long te = (System.currentTimeMillis() - ti) / 1000;
+        System.out.println("tiempo de ejecucion Java es divertido:" + te);
+    }
 }
